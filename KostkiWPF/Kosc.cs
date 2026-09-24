9288f792b96b1036e00fea2e09eff77373d42105
spiorkowski <spiorkowski@S201DELL06>
2026-09-24 08:09:39 +0200

    Dodaj pliki projektów.

---
+++ KostkiWPF/Kosc.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace KostkiWPF
+{
+    public class Kosc
+    {
+        public static int LiczbaInstancji = 0;
+
+        public static string[] NazwyObrazow =
+        {
+            "kosc0.png",
+            "kosc1.png",
+            "kosc2.png",
+            "kosc3.png",
+            "kosc4.png",
+            "kosc5.png",
+            "kosc6.png"
+        };
+
+        public int LiczbaOczek { get; private set; }
+
+        public int IdentyfikatorPliku { get; private set; }
+
+        public bool Dostepna { get; private set; }
+
+        private static Random random = new Random();
+
+        public Kosc(int wartosc)
+        {
+            if (wartosc < 1 || wartosc > 6)
+            {
+                wartosc = 0;
+            }
+
+            LiczbaOczek = wartosc;
+            IdentyfikatorPliku = wartosc;
+            Dostepna = true;
+
+            LiczbaInstancji++;
+        }
+
+        public Kosc()
+        {
+            LiczbaOczek = random.Next(1, 7);
+            IdentyfikatorPliku = LiczbaOczek;
+            Dostepna = true;
+
+            LiczbaInstancji++;
+        }
+
+        public void Rzut()
+        {
+            if (!Dostepna)
+            {
+                return;
+            }
+
+            LiczbaOczek = random.Next(1, 7);
+            IdentyfikatorPliku = LiczbaOczek;
+        }
+
+        public void Zablokuj()
+        {
+            Dostepna = false;
+        }
+
+        public void Odblokuj()
+        {
+            Dostepna = true;
+        }
+
+
+        public string PobierzWartoscTekstowa()
+        {
+            switch (LiczbaOczek)
+            {
+                case 1:
+                    return "jeden";
+
+                case 2:
+                    return "dwa";
+
+                case 3:
+                    return "trzy";
+
+                case 4:
+                    return "cztery";
+
+                case 5:
+                    return "pięć";
+
+                case 6:
+                    return "sześć";
+
+                default:
+                    return "zero";
+            }
+        }
+
+        public string PobierzNazweObrazu()
+        {
+            return NazwyObrazow[IdentyfikatorPliku];
+        }
+    }
+}
---
+++ KostkiWPF/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace KostkiWPF
+{
+    public partial class MainWindow : Window
+    {
+        private Kosc[] kosci;
+
+        private Image[] obrazyKosci;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            kosci = new Kosc[5];
+
+            for (int i = 0; i < kosci.Length; i++)
+            {
+                kosci[i] = new Kosc(0);
+            }
+
+            obrazyKosci = new Image[]
+            {
+                Kosc1,
+                Kosc2,
+                Kosc3,
+                Kosc4,
+                Kosc5
+            };
+        }
+
+        private void RzutButton_Click(object sender, RoutedEventArgs e)
+        {
+            int suma = 0;
+
+            for (int i = 0; i < kosci.Length; i++)
+            {
+                kosci[i].Rzut();
+
+                UstawObrazKosci(
+                    obrazyKosci[i],
+                    kosci[i].LiczbaOczek
+                );
+
+                suma += kosci[i].LiczbaOczek;
+            }
+
+            WynikTextBlock.Text = suma.ToString();
+        }
+
+
+        private void Kosc_Click(object sender, MouseButtonEventArgs e)
+        {
+            Image kliknietaKosc = sender as Image;
+
+            if (kliknietaKosc == null)
+            {
+                return;
+            }
+
+            int indeks = PobierzIndeksKosci(kliknietaKosc);
+
+            if (indeks == -1)
+            {
+                return;
+            }
+
+            if (kosci[indeks].Dostepna)
+            {
+                kosci[indeks].Zablokuj();
+
+                kliknietaKosc.Opacity = 0.5;
+            }
+            else
+            {
+                kosci[indeks].Odblokuj();
+
+                kliknietaKosc.Opacity = 1.0;
+            }
+        }
+
+        private int PobierzIndeksKosci(Image obraz)
+        {
+            if (obraz == Kosc1)
+            {
+                return 0;
+            }
+
+            if (obraz == Kosc2)
+            {
+                return 1;
+            }
+
+            if (obraz == Kosc3)
+            {
+                return 2;
+            }
+
+            if (obraz == Kosc4)
+            {
+                return 3;
+            }
+
+            if (obraz == Kosc5)
+            {
+                return 4;
+            }
+
+            return -1;
+        }
+
+        private void UstawObrazKosci(Image obraz, int wartosc)
+        {
+            string sciezka =
+                $"pack://application:,,,/Images/kosc{wartosc}.png";
+
+            obraz.Source = new BitmapImage(
+                new Uri(sciezka, UriKind.Absolute)
+            );
+        }
+    }
+}
